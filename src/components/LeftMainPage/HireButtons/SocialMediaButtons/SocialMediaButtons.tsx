55446e08574b5e94f36e9f8488b19680514fe703
Bruno Arruda <bruno.arrm@gmail.com>
2023-08-23 21:29:18 -0300

    Botões foram configurados com os respectivos links.

--- src/components/LeftMainPage/HireButtons/SocialMediaButtons/SocialMediaButtons.tsx
+++ src/components/LeftMainPage/HireButtons/SocialMediaButtons/SocialMediaButtons.tsx
@@ -1,18 +1,27 @@
 import styles from '@/components/LeftMainPage/HireButtons/SocialMediaButtons/SocialMediaButtons.module.scss';
-import {BsWhatsapp} from 'react-icons/bs';
-import {FiLinkedin, FiGithub} from 'react-icons/fi';
+import { BsWhatsapp } from 'react-icons/bs';
+import { FiLinkedin, FiGithub } from 'react-icons/fi';
 
 const SocialMediaButtons = () => {
 
     return (
         <div className={styles.socialMediaButtons}>
-            <a className={styles.social} href='www.google.com' target='_blank' rel='noopener noreferer'>
+            <a
+                className={styles.social} target='_blank' rel='noopener noreferer'
+                href='https://wa.me/5562998281602?text=Ol%C3%A1+Bruno%21+Dei+uma+olhada+no+seu+portf%C3%B3lio+e+vim+conversar+com+voc%C3%AA.'
+            >
                 <BsWhatsapp />
             </a>
-            <a className={styles.social} href='www.google.com' target='_blank' rel='noopener noreferer'>
+            <a 
+                className={styles.social} target='_blank' rel='noopener noreferer'
+                href='https://www.linkedin.com/in/bruno-arruda-dev/' 
+            >
                 <FiLinkedin />
             </a>
-            <a className={styles.social} href='www.google.com' target='_blank' rel='noopener noreferer'>
+            <a 
+                className={styles.social} target='_blank' rel='noopener noreferer'
+                href='https://github.com/bruno-arruda-dev' 
+            >
                 <FiGithub />
             </a>
         </div>
