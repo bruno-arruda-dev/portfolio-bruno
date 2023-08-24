8ef26c01cc929c6aec4b0d6bec83e3629e81b7dc
Bruno Arruda <bruno.arrm@gmail.com>
2023-08-24 10:10:34 -0300

    Resolvido problema dos botões da página Contact se movimentarem de acordo com a digitação, ao carregar a página.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -21,7 +21,9 @@ const Contact = () => {
                             repeat={0}
                             cursor={true}
                         />
+
                         <SocialMediaButtons />
+                        
                     </div>
                 </div>
 
