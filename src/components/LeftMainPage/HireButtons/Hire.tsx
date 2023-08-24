bcf19841f460bc0a3a3f77cf47460a2fd51e4479
Bruno Arruda <bruno.arrm@gmail.com>
2023-08-24 10:03:23 -0300

    Legenda do botão Hire foi centralizada corretamente.

--- src/components/LeftMainPage/HireButtons/Hire.tsx
+++ src/components/LeftMainPage/HireButtons/Hire.tsx
@@ -1,6 +1,4 @@
 import styles from '@/components/LeftMainPage/HireButtons/Hire.module.scss';
-import {BsWhatsapp} from 'react-icons/bs';
-import {FiLinkedin, FiGithub} from 'react-icons/fi';
 import SocialMediaButtons from './SocialMediaButtons/SocialMediaButtons';
 
 const Hire = () => {
