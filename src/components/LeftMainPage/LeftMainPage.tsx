2a4e1fbcc4bee241582b4fdf4891acf5cddbef62
Bruno Arruda <bruno.arrm@gmail.com>
2023-08-25 10:24:07 -0300

    Atualizado texto de apresentação da index.

--- src/components/LeftMainPage/LeftMainPage.tsx
+++ src/components/LeftMainPage/LeftMainPage.tsx
@@ -3,6 +3,24 @@ import { TypeAnimation } from 'react-type-animation';
 import Hire from './HireButtons/Hire';
 
 const LeftMainPage = () => {
+    function calcularIdade(dataNascimento: Date): number {
+        const hoje = new Date();
+        const idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+        // Calcular minha idade
+        if (
+            hoje.getMonth() < dataNascimento.getMonth() ||
+            (hoje.getMonth() === dataNascimento.getMonth() && hoje.getDate() < dataNascimento.getDate())
+        ) {
+            return idade - 1;
+        }
+
+        return idade;
+    }
+
+    const dataNascimento = new Date('1989-02-16');
+    const idade = calcularIdade(dataNascimento);
+
 
     return (
         <div className={styles.left_main_page}>
@@ -11,17 +29,17 @@ const LeftMainPage = () => {
 
                 <h1>Olá, meu nome é Bruno</h1>
 
-                <TypeAnimation 
+                <TypeAnimation
                     className={styles.writer}
                     sequence={[
-                        'Acredito que com dedicação...', 1500,
-                        'Acredito que com disciplina...', 900,
-                        'Acredito que com resiliência...', 1200,
-                        'Acredito que é possível programar grandes mudanças na vida das pessoas!'
+                        `Brasileiro, moro em Goiânia. Hoje aos ${idade} anos, sou apaixonado por desenvolvimento web. Em meus projetos gosto de utilizar NextJS, TypeScript e SASS, sempre curioso com novas tecnologias.\n\nAcredito que com foco...`, 100,
+                        `Brasileiro, moro em Goiânia. Hoje aos ${idade} anos, sou apaixonado por desenvolvimento web. Em meus projetos gosto de utilizar NextJS, TypeScript e SASS, sempre curioso com novas tecnologias.\n\nAcredito que com resiliência...`, 1000,
+                        `Brasileiro, moro em Goiânia. Hoje aos ${idade} anos, sou apaixonado por desenvolvimento web. Em meus projetos gosto de utilizar NextJS, TypeScript e SASS, sempre curioso com novas tecnologias.\n\nAcredito que com disciplina...`, 1500,
+                        `Brasileiro, moro em Goiânia. Hoje aos ${idade} anos, sou apaixonado por desenvolvimento web. Em meus projetos gosto de utilizar NextJS, TypeScript e SASS, sempre curioso com novas tecnologias.\n\nAcredito que é possível programar grandes mudanças na vida das pessoas!`
                     ]}
                     wrapper="p"
-                    speed={50}
-                    style={{ display: 'block' }}
+                    speed={90}
+                    style={{whiteSpace: 'pre-line', display: 'block' }}
                     repeat={0}
                     cursor={true}
                 />
