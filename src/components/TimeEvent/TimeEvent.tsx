72bee2d842cdeecd6b7c949bdeddc4f594772c3d
Bruno Arruda <bruno.arrm@gmail.com>
2023-10-10 11:00:45 -0300

    Adicionadas animações ao scrollar na página About.

--- src/components/TimeEvent/TimeEvent.tsx
+++ src/components/TimeEvent/TimeEvent.tsx
@@ -2,11 +2,37 @@ import styles from '@/components/TimeEvent/TimeEvent.module.scss';
 import { ITimeEventProps } from '@/types/TimeEventProps';
 import { GiWhiteBook } from 'react-icons/gi';
 import { BsFillHouseGearFill } from 'react-icons/bs';
+import { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const TimeEvent = ({ type, title, period, descriptions }: ITimeEventProps) => {
+    const thisEvent = useRef(null);
+
+    useLayoutEffect(() => {
+
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to(thisEvent.current, {
+            opacity: 1,
+            x: 0,
+            y: 0,
+            scrollTrigger: {
+                trigger: thisEvent.current,
+                // markers: true,
+                start: 'top 90%',
+                end: 'bottom 90%',
+                scrub: true,
+            }
+        })
+
+        return () => {
+            gsap.killTweensOf(thisEvent.current)
+        }
+
+    }, [])
 
     return (
-        <div className={`${styles['eventContainer']} ${styles['eventContainer_' + type]}`}>
+        <div className={`${styles['eventContainer']} ${styles['eventContainer_' + type]}`} ref={thisEvent}>
             <div className={styles.studyInfo}>
                 <h2>{title}</h2>
                 <p>{period}</p>
